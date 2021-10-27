5b5fc2f44f668c02679f254a71f48ec296ff1f84
Max.k <maxim.k.elasnce@gmail.com>
2021-10-27 14:12:51 +0300

    Async test

---
+++ async/mutex.cpp
@@ -0,0 +1,23 @@
+﻿#include <thread>
+#include <iostream>
+#include <string>
+#include <mutex>
+
+void run(std::string value, std::mutex *d) 
+{
+    
+    d->lock();
+    std::cout << value << std::endl;
+    
+    d->unlock();
+}
+
+int main() {
+    std::mutex d;
+    for (int i = 0; i < 200; i++) {
+        std::thread tA(run, "A",&d);
+        std::thread tB(run, "B", &d);
+        tA.join();
+        tB.join();
+    }
+}
